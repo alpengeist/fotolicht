60984d22faabaea1d35b13d54692bfe4b4567f09
alpen <alpen.hermann@gmail.com>
2026-03-13 20:30:08 +0100

    initial

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,8 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-}
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
