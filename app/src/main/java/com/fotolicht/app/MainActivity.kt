2255c0e5b8f096423a404e728fadad35bf112f31
alpen <alpen.hermann@gmail.com>
2026-03-14 13:33:35 +0100

    add navigation bars padding to brightness slider

--- app/src/main/java/com/fotolicht/app/MainActivity.kt
+++ app/src/main/java/com/fotolicht/app/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -103,6 +104,7 @@ private fun LightScreen() {
                 onBrightnessChange = { brightness = it },
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
+                    .navigationBarsPadding()
                     .padding(20.dp)
             )
         }
